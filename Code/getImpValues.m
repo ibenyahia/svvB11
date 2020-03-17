8026003ca0b7e329ba5e22560b72c7cf8216fecd
ibenyahia <i.benyahia@hotmail.com>
2020-03-17 13:15:08 +0100

    Found the curves, returned the Cit par file to normal and made a new file for the first and second meas. Made small change to cg_flightwrittenvalues and getImpvalues

--- Code/getImpValues.m
+++ Code/getImpValues.m
@@ -2,7 +2,7 @@ function [rho,p,V_EAS,V_TAS,delta_T,M_T] = getImpValues(h_p,V_CAS,T_meas)
     
 % constants parameters:
     R_c = 287.05;
-    g = 9.81;
+    g = 9.80665;
     T_0 = 288.15;
     gamma = 1.4;
     rho_0 = 1.225;
@@ -10,10 +10,8 @@ function [rho,p,V_EAS,V_TAS,delta_T,M_T] = getImpValues(h_p,V_CAS,T_meas)
     lam = -0.0065;
 % calculations
     T_ISA = T_0 + lam*h_p;
-    for i = 1:length(h_p)
     rho = rho_0*((1+(lam*h_p/T_0))).^(-((g/(lam*R_c))+1));
-    end
-    p = R_c*rho.*T_meas;
+    p = p_0*(1+lam*h_p/T_0).^(-g/(lam*R_c));
     M_T = zeros(1,length(p));
     for i=1:length(p)
     M_T(:,i) = sqrt(2/(gamma-1)*((1+p_0/p(i)*((1+(gamma-1)/(2*gamma) * rho_0/p_0 * V_CAS(i)^2)^(gamma/(gamma-1))-1))^((gamma-1)/gamma)-1));
