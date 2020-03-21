286ca8bea96c6ea6425ad2e3b28f238beaf806b4
ibenyahia <i.benyahia@hotmail.com>
2020-03-21 14:38:11 +0100

    I put everything in their map

--- Code/NUMERICAL_MODEL/NumericalModel.m
+++ Code/NUMERICAL_MODEL/NumericalModel.m
@@ -91,7 +91,7 @@ depsda = 4/(A+2);               % Downwash gradient [ ]
 % Lift and drag coefficient
 
 CL = 2*W/(rho*V0^2*S);               % Lift coefficient [ ]
-CD = CD0 + (CLa*alpha0)^2/(pi*A*e);  % Drag coefficient [ ]
+CD = CD0 + (CL)^2/(pi*A*e);  % Drag coefficient [ ]
 
 % Stabiblity derivatives
 
