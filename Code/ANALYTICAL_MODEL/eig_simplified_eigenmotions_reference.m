133cd3158e599427e33898abd7e5e4b7379dfc9b
Tim <t.a.vanvoorbergen@gmail.com>
2020-03-21 16:11:03 +0100

    smoll changes to be able to view eigenvalues in command window

--- Code/ANALYTICAL_MODEL/eig_simplified_eigenmotions_reference.m
+++ Code/ANALYTICAL_MODEL/eig_simplified_eigenmotions_reference.m
@@ -7,14 +7,14 @@ A_SPM = 2*muc_SPM*KY2*(2*muc_SPM-CZadot);
 B_SPM = -2*muc_SPM*KY2*CZa-(2*muc_SPM+CZq)*Cmadot-(2*muc_SPM-CZadot)*Cmq;
 C_SPM = CZa*Cmq-(2*muc_SPM+CZq)*Cma;
 
-Lambda_1s = (-B_SPM+sqrt(-4*A_SPM*C_SPM+B_SPM^2))/(2*A_SPM);
+Lambda_1s = (-B_SPM+sqrt(-4*A_SPM*C_SPM+B_SPM^2))/(2*A_SPM)
 %Lambda_1sscalar = sqrt(real(Lambda_1s)^2+imag(Lambda_1s)^2)
 
 %P_SPM1 = (2*pi/imag(Lambda_1s))*(c/V)
 %T_SPM1 = (ln(1/2)/Lambda_1sscalar)*(c/V)
 %D_SPM1 = -real(Lambda_1s)/(Lambda_1sscalar)
 
-Lambda_2s = (-B_SPM-sqrt(-4*A_SPM*C_SPM+B_SPM^2))/(2*A_SPM);
+Lambda_2s = (-B_SPM-sqrt(-4*A_SPM*C_SPM+B_SPM^2))/(2*A_SPM)
 %Lambda_2sscalar = sqrt(real(Lambda_2s)^2+imag(Lambda_2s)^2)
 
 %P_SPM2 = (2*pi/imag(Lambda_2s))*(c/V)
@@ -27,14 +27,14 @@ A_PHU = 2*muc_PHU*(CZa*Cmq-2*muc_PHU*Cma);
 B_PHU = 2*muc_PHU*(CXu*Cma-Cmu*CXa)+Cmq*(CZu*CXa-CXu*CZa);
 C_PHU = CZ0_PHU*(Cmu*CZa-CZu*Cma);
 
-Lambda_3s = (-B_PHU+sqrt(-4*A_PHU*C_PHU+B_PHU^2))/(2*A_PHU);
+Lambda_3s = (-B_PHU+sqrt(-4*A_PHU*C_PHU+B_PHU^2))/(2*A_PHU)
 %Lambda_3sscalar = sqrt(real(Lambda_3s)^2+imag(Lambda_3s)^2)
 
 %P_PHU1 = (2*pi/imag(Lambda_3s))*(c/V)
 %T_PHU1 = (ln(1/2)/Lambda_3sscalar)*(c/V)
 %D_PHU1 = -real(Lambda_3s)/(Lambda_3sscalar)
 
-Lambda_4s = (-B_PHU-sqrt(-4*A_PHU*C_PHU+B_PHU^2))/(2*A_PHU);
+Lambda_4s = (-B_PHU-sqrt(-4*A_PHU*C_PHU+B_PHU^2))/(2*A_PHU)
 %Lambda_4sscalar = sqrt(real(Lambda_4s)^2+imag(Lambda_4s)^2)
 
 %P_PHU2 = (2*pi/imag(Lambda_4s))*(c/V)
@@ -48,32 +48,35 @@ Lambda_4s = (-B_PHU-sqrt(-4*A_PHU*C_PHU+B_PHU^2))/(2*A_PHU);
 
 %Aperiodic roll motion
 
-Lambda_1a = Clp/(4*mub_ARM*KX2);
+Lambda_1a = Clp/(4*mub_ARM*KX2)
 %Lambda_1ascalar = sqrt(real(Lambda_1a)^2+imag(Lambda_1a)^2)
 
 %P_ARM = (2*pi/imag(Lambda_1a))*(c/V)
 %T_ARM = (ln(1/2)/Lambda_1ascalar)*(c/V)
 %D_ARM = -real(Lambda_1a)/(Lambda_1ascalar)
 
-%Dutch roll
+%Dutch roll (only use DR not DRDAMP, bcs no equations for that one)
+A_DR = 8*(mub_DR^2)*KZ2 ;
+B_DR = -2*mub_DR*(Cnr+2*KZ2*CYb) ;
+C_DR = (4*mub_DR*Cnb)+(CYb*Cnr) ;
 
-Lambda_2a = 2*(Cnr+2*KZ2*CYb)+sqrt(-64*KZ2*(4*mub_DR*Cnb+CYb*Cnr)+4*(Cnr+2*KZ2*CYb)^2)/(16*mub_DR*KZ2);
+Lambda_2a = (-B_DR+sqrt(-(4*A_DR*C_DR)+(B_DR^2)))/(2*A_DR)
 %Lambda_2ascalar = sqrt(real(Lambda_2a)^2+imag(Lambda_2a)^2)
 
-%P_DRM1 = (2*pi/imag(Lambda_2a))*(c/V)
-%T_DRM1 = (ln(1/2)/Lambda_2ascalar)*(c/V)
+%P_DRM1 = (2*pi/imag(Lambda_2a))*(c/V0(4))
+%T_DRM1 = (log(1/2)/Lambda_2ascalar)*(c/V0(4))
 %D_DRM1 = -real(Lambda_2a)/(Lambda_2ascalar)
 
-Lambda_3a = 2*(Cnr+2*KZ2*CYb)-sqrt(-64*KZ2*(4*mub_DR*Cnb+CYb*Cnr)+4*(Cnr+2*KZ2*CYb)^2)/(16*mub_DR*KZ2);
+Lambda_3a = (-B_DR-sqrt(-(4*A_DR*C_DR)+(B_DR^2)))/(2*A_DR)
 %Lambda_3ascalar = sqrt(real(Lambda_3a)^2+imag(Lambda_3a)^2)
 
-%P_DRM2 = (2*pi/imag(Lambda_3a))*(c/V)
-%T_DRM2 = (ln(1/2)/Lambda_3ascalar)*(c/V)
+%P_DRM2 = (2*pi/imag(Lambda_3a))*(c/V0(4))
+%T_DRM2 = (log(1/2)/Lambda_3ascalar)*(c/V0(4))
 %D_DRM2 = -real(Lambda_3a)/(Lambda_3ascalar)
 
 %Spiral motion
 
-Lambda_4a = (2*CL_SM*(Clb*Cnr-Cnb*Clr))/(Clp*(CYb*Cnr+4*mub_SM*Cnb)-Cnp*(CYb*Clr+4*mub_SM*Clb));
+Lambda_4a = (2*CL_SM*(Clb*Cnr-Cnb*Clr))/(Clp*(CYb*Cnr+4*mub_SM*Cnb)-Cnp*(CYb*Clr+4*mub_SM*Clb))
 %Lambda_4ascalar = sqrt(real(Lambda_4a)^2+imag(Lambda_4a)^2)
 
 %P_SM = (2*pi/imag(Lambda_4a))*(c/V)
