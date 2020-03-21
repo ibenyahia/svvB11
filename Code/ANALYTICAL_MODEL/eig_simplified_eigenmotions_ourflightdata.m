fb5357c663e06dfb8b2ba0672e2cc10e8ba2cce6
Tim <t.a.vanvoorbergen@gmail.com>
2020-03-21 16:51:04 +0100

    calculating period, damping etc...

--- Code/ANALYTICAL_MODEL/eig_simplified_eigenmotions_ourflightdata.m
+++ Code/ANALYTICAL_MODEL/eig_simplified_eigenmotions_ourflightdata.m
@@ -8,19 +8,19 @@ B_SPM = -2*muc_SPM*KY2*CZa-(2*muc_SPM+CZq)*Cmadot-(2*muc_SPM-CZadot)*Cmq;
 C_SPM = CZa*Cmq-(2*muc_SPM+CZq)*Cma;
 
 Lambda_1s = (-B_SPM+sqrt(-(4*A_SPM*C_SPM)+(B_SPM^2)))/(2*A_SPM)
-%Lambda_1sscalar = sqrt(real(Lambda_1s)^2+imag(Lambda_1s)^2)
+Lambda_1sscalar = sqrt(real(Lambda_1s)^2+imag(Lambda_1s)^2);
 
 % use V0 for V???
-%P_SPM1 = (2*pi/imag(Lambda_1s))*(c/V0(1))
-%T_SPM1 = (lnog1/2)/Lambda_1sscalar)*(c/V0(1))
-%D_SPM1 = -real(Lambda_1s)/(Lambda_1sscalar)
+P_SPM1 = (2*pi/imag(Lambda_1s))*(c/V0(1));
+T_SPM1 = (log(1/2)/Lambda_1sscalar)*(c/V0(1));
+D_SPM1 = -real(Lambda_1s)/(Lambda_1sscalar);
 
 Lambda_2s = (-B_SPM-sqrt(-(4*A_SPM*C_SPM)+(B_SPM^2)))/(2*A_SPM)
-%Lambda_2sscalar = sqrt(real(Lambda_2s)^2+imag(Lambda_2s)^2)
+Lambda_2sscalar = sqrt(real(Lambda_2s)^2+imag(Lambda_2s)^2);
 
-%P_SPM2 = (2*pi/imag(Lambda_2s))*(c/V0(1))
-%T_SPM2 = (log(1/2)/Lambda_2sscalar)*(c/V0(1))
-%D_SPM2 = -real(Lambda_2s)/(Lambda_2sscalar)
+P_SPM2 = (2*pi/imag(Lambda_2s))*(c/V0(1));
+T_SPM2 = (log(1/2)/Lambda_2sscalar)*(c/V0(1));
+D_SPM2 = -real(Lambda_2s)/(Lambda_2sscalar);
 
 
 %Phugoid
@@ -29,18 +29,18 @@ B_PHU = 2*muc_PHU*(CXu*Cma-Cmu*CXa)+Cmq*(CZu*CXa-CXu*CZa);
 C_PHU = CZ0_PHU*(Cmu*CZa-CZu*Cma);
 
 Lambda_3s = (-B_PHU+sqrt(-4*A_PHU*C_PHU+B_PHU^2))/(2*A_PHU)
-%Lambda_3sscalar = sqrt(real(Lambda_3s)^2+imag(Lambda_3s)^2)
+Lambda_3sscalar = sqrt(real(Lambda_3s)^2+imag(Lambda_3s)^2)
 
-%P_PHU1 = (2*pi/imag(Lambda_3s))*(c/V0(2))
-%T_PHU1 = (log(1/2)/Lambda_3sscalar)*(c/V0(2))
-%D_PHU1 = -real(Lambda_3s)/(Lambda_3sscalar)
+P_PHU1 = (2*pi/imag(Lambda_3s))*(c/V0(2))
+T_PHU1 = (log(1/2)/Lambda_3sscalar)*(c/V0(2))
+D_PHU1 = -real(Lambda_3s)/(Lambda_3sscalar)
 
 Lambda_4s = (-B_PHU-sqrt(-4*A_PHU*C_PHU+B_PHU^2))/(2*A_PHU)
-%Lambda_4sscalar = sqrt(real(Lambda_4s)^2+imag(Lambda_4s)^2)
+Lambda_4sscalar = sqrt(real(Lambda_4s)^2+imag(Lambda_4s)^2)
 
-%P_PHU2 = (2*pi/imag(Lambda_4s))*(c/V0(2))
-%T_PHU2 = (log(1/2)/Lambda_4sscalar)*(c/V0(2))
-%D_PHU2 = -real(Lambda_4s)/(Lambda_4sscalar)
+P_PHU2 = (2*pi/imag(Lambda_4s))*(c/V0(2));
+T_PHU2 = (log(1/2)/Lambda_4sscalar)*(c/V0(2));
+D_PHU2 = -real(Lambda_4s)/(Lambda_4sscalar);
 
 
 
@@ -62,18 +62,18 @@ B_DR = -2*mub_DR*(Cnr+2*KZ2*CYb) ;
 C_DR = (4*mub_DR*Cnb)+(CYb*Cnr) ;
 
 Lambda_2a = (-B_DR+sqrt(-(4*A_DR*C_DR)+(B_DR^2)))/(2*A_DR)
-%Lambda_2ascalar = sqrt(real(Lambda_2a)^2+imag(Lambda_2a)^2)
+Lambda_2ascalar = sqrt(real(Lambda_2a)^2+imag(Lambda_2a)^2);
 
-%P_DRM1 = (2*pi/imag(Lambda_2a))*(c/V0(4))
-%T_DRM1 = (log(1/2)/Lambda_2ascalar)*(c/V0(4))
-%D_DRM1 = -real(Lambda_2a)/(Lambda_2ascalar)
+P_DRM1 = (2*pi/imag(Lambda_2a))*(c/V0(4));
+T_DRM1 = (log(1/2)/Lambda_2ascalar)*(c/V0(4));
+D_DRM1 = -real(Lambda_2a)/(Lambda_2ascalar);
 
 Lambda_3a = (-B_DR-sqrt(-(4*A_DR*C_DR)+(B_DR^2)))/(2*A_DR)
-%Lambda_3ascalar = sqrt(real(Lambda_3a)^2+imag(Lambda_3a)^2)
+Lambda_3ascalar = sqrt(real(Lambda_3a)^2+imag(Lambda_3a)^2);
 
-%P_DRM2 = (2*pi/imag(Lambda_3a))*(c/V0(4))
-%T_DRM2 = (log(1/2)/Lambda_3ascalar)*(c/V0(4))
-%D_DRM2 = -real(Lambda_3a)/(Lambda_3ascalar)
+P_DRM2 = (2*pi/imag(Lambda_3a))*(c/V0(4));
+T_DRM2 = (log(1/2)/Lambda_3ascalar)*(c/V0(4));
+D_DRM2 = -real(Lambda_3a)/(Lambda_3ascalar);
 
 %Spiral motion
 
