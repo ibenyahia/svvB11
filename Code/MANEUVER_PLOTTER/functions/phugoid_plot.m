4bc53aeb75a673c15d7d103d87c1a987fd2ac67a
Herrberth <herrberth@gmx.net>
2020-03-22 19:14:04 +0100

    adjusted phug plot

--- Code/MANEUVER_PLOTTER/functions/phugoid_plot.m
+++ Code/MANEUVER_PLOTTER/functions/phugoid_plot.m
@@ -1,8 +1,8 @@
-function [] = phugoid_plot(V_dat, h_dat,theta_dat,delta_dat, t_dat, V_mod, h_mod, theta_mod, delta_mod,  t_mod)
+function [] = phugoid_plot(vtas_dat, theta_dat, delta_dat, q_dat, t_dat, vtas_mod, theta_mod, q_mod,  t_mod)
 %plotter for short phugoid
 %just type phugoid_plot and plug in your data zack beautiful graph
 %_dat for flight/ref data, _mod for modeled data
-%V = spedd, h = altutude theta = pitch ,delta = elevator pitch, t = time 
+%vtas = true air spedd, theta = pitch ,delta = elevator pitch, q = pitch rate, t = time 
 
 figure;
 sgtitle('Phugoid')
@@ -10,30 +10,30 @@ sgtitle('Phugoid')
 subplot(2,2,1);
 plot(t_dat,theta_dat,'r',t_mod,theta_mod,'b')
 title('pitch vs t')
-ylabel('[deg]')
-xlabel('[sec]')
+ylabel('[rad]')
+xlabel('[s]')
 legend('Flight Data','Model Data')
 
 subplot(2,2,3);
-plot(t_dat,h_dat,'r',t_mod,h_mod,'b')
-title('altitude vs t')
-ylabel('[km]')
-xlabel('[sec]')
-legend('Flight Data','Model Data')
+plot(t_dat,q_dat,'r',t_mod,q_mod,'b')
+title('pitch rate vs t')
+ylabel('[rad]')
+xlabel('[s]')
 
-subplot(2,2,2);
-plot(t_dat,delta_dat,'r',t_mod,delta_mod,'b')
-title('delta_e vs t')
-ylabel('[deg]')
-xlabel('[sec]')
-legend('Flight Data','Model Data')
 
 subplot(2,2,4);
-plot(t_dat,V_dat,'r',t_mod,V_mod,'b')
+plot(t_dat,delta_dat,'g')
+title('delta_e vs t')
+ylabel('[deg]')
+xlabel('[s]')
+
+
+subplot(2,2,2);
+plot(t_dat,vtas_dat,'r',t_mod,vtas_mod,'b')
 title('Speed vs t')
 ylabel('[m/s]')
-xlabel('[sec]')
-legend('Flight Data','Model Data')
+xlabel('[s]')
+
 
 
 end
--- Code/MANEUVER_PLOTTER/functions/short_period_plot.m
+++ Code/MANEUVER_PLOTTER/functions/short_period_plot.m
@@ -1,4 +1,4 @@
-function [] = short_period_plot(alpha_dat,theta_dat, delta_dat,q_dat, t_dat, alpha_mod, delta_mod,q_mod, t_mod)
+function [] = short_period_plot(alpha_dat,theta_dat, delta_dat, q_dat, t_dat, alpha_mod,q_mod, t_mod)
 %plotter for short period eigenmotion
 %just type short_period_plot and plug in your data zack beautiful graph
 %_dat for flight/ref data, _mod for modeled data
@@ -20,7 +20,7 @@ ylabel('[rad/s]')
 xlabel('[s]')
 
 subplot(2,2,4);
-plot(t_dat,delta_dat,'g',)
+plot(t_dat, delta_dat,'g')
 title('delta_e vs t')
 ylabel('[rad]')
 xlabel('[s]')
